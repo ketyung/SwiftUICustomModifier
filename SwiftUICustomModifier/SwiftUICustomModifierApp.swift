99278559548a7d3c115304a36a72696871c2626a
Christopher Chee <ketyung@techchee.com>
2021-01-15 08:00:32 +0800

    Initial Commit

---
+++ SwiftUICustomModifier/SwiftUICustomModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICustomModifierApp.swift
+//  SwiftUICustomModifier
+//
+//  Created by Chee Ket Yung on 15/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICustomModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
